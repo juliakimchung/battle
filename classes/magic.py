c81617438a293e16154bde552260800b818f926c
juliakimchung <julhkm@gmail.com>
2017-04-18 09:17:02 -0500

    spell created

--- classes/magic.py
+++ classes/magic.py
@@ -1,6 +1,13 @@
+import random
+
 class Spell:
-	def __init_-(self, name, cost, dmg, types):
-		self.name = name
-		self.cost = cost
-		self.dmg = dmg
-		self.type = types
+    def __init__(self, name, cost, dmg, types):
+        self.name = name
+        self.cost = cost
+        self.dmg = dmg
+        self.type = types
+
+    def generate_damage(self):
+        low = self.dmg - 15
+        high = self.dmg + 15
+        return random.randrange(low, high)
